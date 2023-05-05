c29780fb0242d3fc6c7c291996e3266c5632310d
Faisal Saleem <saleemfs27@gmail.com>
2023-05-05 20:47:18 +0200

    Init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { type AppType } from "next/dist/shared/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import "@/styles/globals.css";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <ClerkProvider>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+};
+
+export default MyApp;
