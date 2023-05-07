86509d682a2b2ca88e9ecbf4c7a04196b9908739
Faisal Saleem <saleemfs27@gmail.com>
2023-05-07 22:04:57 +0200

    Clerk config

--- src/middlware.ts
+++ src/middlware.ts
@@ -1,6 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+  afterAuth(auth, req, evt) {
+    console.log(auth);
+  },
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)"],
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 import "@/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider>
+    <ClerkProvider {...pageProps} appearance={{ baseTheme: dark }}>
       <Component {...pageProps} />
     </ClerkProvider>
   );
